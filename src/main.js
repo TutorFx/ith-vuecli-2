425d6041a7e25f0ed42da25d5e49d0840ab8638a
TutorFx <gabriel_serejo@live.com>
2021-03-10 12:11:37 -0300

    Axios

--- src/main.js
+++ src/main.js
@@ -3,6 +3,16 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 import './registerServiceWorker'
+import axios from 'axios'
+
+Vue.prototype.$search = ''
+Vue.prototype.$cursos = []
+
+axios.get('https://api.ithpos.com.br/wp-json/ith/v1/cursos/').then((resposta) => {
+  Vue.prototype.$cursos = resposta.data;
+}).catch((error) => {
+  console.log(error);
+})
 
 Vue.config.productionTip = false
 
