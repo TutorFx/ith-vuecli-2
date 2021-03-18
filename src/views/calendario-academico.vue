86c62b453d25b00aaf1d3ddc3aefd7fdcd3ef652
Ian-Alves-Nascimento <80347428+Ian-Alves-Nascimento@users.noreply.github.com>
2021-03-18 19:46:22 -0300

    bug

---
+++ src/views/calendario-academico.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="calendario-academico">
+    <layout>
+      <slider />
+      <section-four />
+    </layout>
+  </div>
+</template>
+
+<style scoped lang="scss">
+  .slider-container{
+    background-color: #000;
+  }
+</style>
+
+<script>
+import layout from "@/components/Layout.vue"
+import slider from "@/components/home/slider"
+import sectionFour from "@/components/trabalhe-conosco/sectionfour.vue"
+
+
+export default {
+  name: 'calendario-academico',
+  components: {
+    layout, slider, sectionFour
+    
+  },
+}
+</script>
