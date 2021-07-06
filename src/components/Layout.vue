c432f7c0c689627cdea885d0c7b04f92677ff639
TutorFx <gabriel_serejo@live.com>
2021-07-06 14:09:05 -0300

    Revert "Update Layout.vue"
    This reverts commit b4a784955c1953f0e1b637c21a5e9f745cbd5d62.

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -2,6 +2,12 @@
   <div>
     <navbar-one :class="storage.contraste ? 'contraste':''" />
       <slot id="content" ></slot>
+      <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+          <div class="vw-plugin-top-wrapper"></div>
+        </div>
+      </div>
     <footer-one :class="storage.contraste ? 'contraste':''" />
   </div>
 </template>
@@ -11,9 +17,15 @@
 }
 </style>
 <script>
+import VueScript2 from 'vue-script2'
 import navbarOne from "@/components/navigation/Navbar.vue";
 import footerOne from "@/components/navigation/Footer.vue";
 export default {
       components: { navbarOne, footerOne },
+      mounted(){
+        VueScript2.load('https://vlibras.gov.br/app/vlibras-plugin.js').then(function () {
+          new window.VLibras.Widget('https://vlibras.gov.br/app');
+        })
+      }
 }
 </script>
